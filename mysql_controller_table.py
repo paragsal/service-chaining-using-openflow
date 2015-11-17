1d5bab88c49b18d3b0c7943f0463ec2d0fdab45a
sjha3 <sjha3@ncsu.edu>
2015-11-16 18:29:24 -0800

    uncommenting table creation and port insertion part

--- mysql_controller_table.py
+++ mysql_controller_table.py
@@ -11,17 +11,17 @@ cursor = db.cursor()
 
 # Drop table if it already exist using execute() method.
 #cursor.execute("DROP TABLE IF EXISTS SWITCH1")
-'''
+
 # Create table as per requirement
 sql = """CREATE TABLE CONTROLLER (
          SERVICE  VARCHAR(80) NOT NULL UNIQUE,
          PROVIDER VARCHAR(80),SWITCH VARCHAR(100))"""
 
 cursor.execute(sql)
-'''
+
 #y='10.0.0.2'
 
-#cursor.execute("ALTER TABLE CONTROLLER ADD COLUMN PORT VARCHAR(40)");
+cursor.execute("ALTER TABLE CONTROLLER ADD COLUMN PORT VARCHAR(40)");
 cursor.execute("INSERT INTO CONTROLLER(SERVICE,PROVIDER,SWITCH,PORT) VALUES('EL','10.0.1.2','00:00:00:00:00:00:00:01','s4-eth2')")
 
 cursor.execute("INSERT INTO CONTROLLER(SERVICE,PROVIDER,SWITCH,PORT) VALUES('EM','10.0.0.4,10.0.0.5','00:00:00:00:00:00:00:02','s4-eth2')")
