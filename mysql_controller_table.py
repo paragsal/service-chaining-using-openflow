d6a1123046a1cadd4480662769286415cb2df3b2
sjha3 <sjha3@ncsu.edu>
2015-11-25 21:58:57 -0800

    changed database with different output and input port for service VMs

--- mysql_controller_table.py
+++ mysql_controller_table.py
@@ -4,7 +4,13 @@ import MySQLdb
 import socket, struct
 # Open database connection
 
-db = MySQLdb.connect("localhost","testuser","test623","testdb1" )
+#db = MySQLdb.connect("localhost","testuser","test623","testdb1" )
+
+db = MySQLdb.connect(host="localhost",user="root",passwd="root")
+db1 = db.cursor()
+db1.execute('CREATE DATABASE test2')
+db1.execute("USE test2");
+
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
@@ -13,45 +19,24 @@ cursor = db.cursor()
 #cursor.execute("DROP TABLE IF EXISTS SWITCH1")
 
 # Create table as per requirement
-sql = """CREATE TABLE CONTROLLER (
+
+sql = """CREATE TABLE CONTROLLER2 (
          SERVICE  VARCHAR(80) NOT NULL UNIQUE,
-         PROVIDER VARCHAR(80),SWITCH VARCHAR(100))"""
+         PROVIDER VARCHAR(80),SWITCH VARCHAR(80),PORT VARCHAR(80))"""
 
-cursor.execute(sql)
+#cursor.execute(sql)
 
+cursor.execute("CREATE TABLE CONTROLLER2 (SERVICE VARCHAR(80) NOT NULL UNIQUE, PROVIDER VARCHAR(80),SWITCH VARCHAR(80),OUTPORT VARCHAR(80),INPORT VARCHAR(80))")
 #y='10.0.0.2'
 
-cursor.execute("ALTER TABLE CONTROLLER ADD COLUMN PORT VARCHAR(40)");
-cursor.execute("INSERT INTO CONTROLLER(SERVICE,PROVIDER,SWITCH,PORT) VALUES('EL','10.0.1.2','00:00:00:00:00:00:00:01','s4-eth2')")
+#cursor.execute("ALTER TABLE CONTROLLER ADD COLUMN PORT VARCHAR(40)");
+#10.0.0.6 is Srvc1 <-> SW5 -- F1
+#
+cursor.execute("INSERT INTO CONTROLLER2(SERVICE,PROVIDER,SWITCH,OUTPORT,INPORT) VALUES('F1','10.0.0.6,10.0.0.5','5,4','3,2','4,3')")
+ 
+cursor.execute("INSERT INTO CONTROLLER2(SERVICE,PROVIDER,SWITCH,OUTPORT,INPORT) VALUES('EC','10.0.0.4','4','2','2')")
 
-cursor.execute("INSERT INTO CONTROLLER(SERVICE,PROVIDER,SWITCH,PORT) VALUES('EM','10.0.0.4,10.0.0.5','00:00:00:00:00:00:00:02','s4-eth2')")
-
-cursor.execute("SELECT * FROM CONTROLLER WHERE SERVICE='EB'");
-rows=cursor.fetchall()
-
-print cursor.rowcount
-for row in rows:
-    print row
-
-cursor.execute("SELECT PROVIDER,SWITCH FROM CONTROLLER WHERE SERVICE='EG'");
-row=cursor.fetchone()
-print row[0];
-x=row[0]
-print "copied value of row[0]"
-print x;
-my=x.split(',');
-print "length of array after splitting"
-print len(my)
-print "different value which were separated by comma"
-print my[0]
-print my[1]
-
-print "SWITCH DPID"
-print row[1]
-
-
+cursor.execute("INSERT INTO CONTROLLER2(SERVICE,PROVIDER,SWITCH,OUTPORT,INPORT) VALUES('F2','10.0.0.4','5','3','4')")
 db.commit()
-
-
 # disconnect from server
 db.close()
